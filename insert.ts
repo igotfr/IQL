569a4a8998020d1dc8568f96519f62d71cc2e7d8
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2020-10-31 06:05:45 -0300

    Update insert.ts

--- insert.ts
+++ insert.ts
@@ -1,11 +1,11 @@
-insert.database("app").collection("Users").document(
+database("app").collection("Users").insertDocument(
 {
   _id: ID(5ds564654fd),
   nome: "Adalberto",
   idade: 48
 });
 
-insert.database("app").collection("Users").document([
+database("app").collection("Users").insertDocument([
 {
   _id: ID(df4547454h),
   nome: "Ronaldo",
