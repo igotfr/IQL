d450abbfa173dd12c67e5fe71e73f3c979ebd3f4
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2020-10-31 06:20:52 -0300

    Update find.ts

--- find.ts
+++ find.ts
@@ -1,3 +1,3 @@
 database("app").collection("Users").findDocument(user -> user.nome == "Ronaldo" && user.idade > 8)
 
-database("app").collection("Users").findDocument({nome, idade} -> nome == "Ronaldo" && idade > 8)
+database("app").collection("Users").findDocument(({ nome, idade }) -> nome == "Ronaldo" && idade > 8)
