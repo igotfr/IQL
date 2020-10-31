806df7e7d551cf2c808c13e7344f0ba8e0c51e5e
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2020-10-31 05:15:50 -0300

    Update create.ts

--- create.ts
+++ create.ts
@@ -1,3 +1,9 @@
 create.database("app");
 
+create.database(["app", "barbearia"]);
+
+__________________________________________________________
+
 create.database("app").collection("Users");
+
+create.database("app").collection(["Users", "Products"]);
