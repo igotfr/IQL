0c4765de4737310bc42760f824f16c778c449df6
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2020-11-18 14:28:08 -0300

    Update delete.ts

--- delete.ts
+++ delete.ts
@@ -1,6 +1,6 @@
-database("app").collection("Users").deleteDocument(user -> user.nome == "Ronaldo" && user.idade > 8);
+database("app").collection("Users").deleteDocument(user -> user.nome == "Ronaldo" && user.idade > 8, [_id, nome]); // the second parameter is optional and works like in GraphQL)
 
-database("app").collection("Users").deleteDocument(({ nome, idade }) -> nome == "Ronaldo" && idade > 8);
+database("app").collection("Users").deleteDocument(({ nome, idade }) -> nome == "Ronaldo" && idade > 8, [_id, nome]); // the second parameter is optional and works like in GraphQL)
 
 ___________________________________________________________________________________________________________________________________________________
 
