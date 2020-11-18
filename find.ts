fcc1c1fe407e0e137ec0766570acc4f5a6a2329f
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2020-11-18 13:07:06 -0300

    Update find.ts

--- find.ts
+++ find.ts
@@ -1,8 +1,9 @@
 database("app").collection("Users").findDocument(user -> user.nome == "Ronaldo" && user.idade > 8, { _id, nome }); // the second parameter is optional and works like in GraphQL)
 
 database("app").collection("Users").findDocument(({ nome, idade }) -> nome == "Ronaldo" && idade > 8, { _id, nome }); // the second parameter is optional and works like in GraphQL)
-
+```
 ___
+```
 
 database("app").collection("Users").findDocument([
   { user -> user.nome == "Ronaldo" && user.idade > 8, { _id, nome } },
