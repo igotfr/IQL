bb49ac31896b13b76827de3c505aefcc94869695
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2020-11-18 14:31:42 -0300

    Update find.ts

--- find.ts
+++ find.ts
@@ -9,10 +9,10 @@ database("app").collection("Users").findDocument([
   [ user -> user.nome == "Ronaldo" && user.idade > 8, [_id, nome] ],
   [ user -> user.idade > 8, [nome, idade] ],
   user -> user.nome == "Meterssaker"
-], [_id, nome]); // for not specified, in this case: "Meterssaker"
+], [_id, nome]); // for not specifieds, in this case: "Meterssaker"
 
 database("app").collection("Users").findDocument([
   [ ({ nome, idade }) -> nome == "Ronaldo" && idade > 8, [_id, nome] ],
   [ ({ idade }) -> idade > 8, [nome, idade] ],
   ({ nome }) -> nome == "Meterssaker"
-], [_id, nome]); // for not specified, in this case: "Meterssaker"
+], [_id, nome]); // for not specifieds, in this case: "Meterssaker"
