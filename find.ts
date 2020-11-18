2749e877332dceb4878baba848213346ebeb3f6f
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2020-11-18 13:46:01 -0300

    Update find.ts

--- find.ts
+++ find.ts
@@ -1,16 +1,16 @@
-database("app").collection("Users").findDocument(user -> user.nome == "Ronaldo" && user.idade > 8, { _id, nome }); // the second parameter is optional and works like in GraphQL)
+database("app").collection("Users").findDocument(user -> user.nome == "Ronaldo" && user.idade > 8, [_id, nome]); // the second parameter is optional and works like in GraphQL)
 
-database("app").collection("Users").findDocument(({ nome, idade }) -> nome == "Ronaldo" && idade > 8, { _id, nome }); // the second parameter is optional and works like in GraphQL)
+database("app").collection("Users").findDocument(({ nome, idade }) -> nome == "Ronaldo" && idade > 8, [_id, nome]); // the second parameter is optional and works like in GraphQL)
 
 ___________________________________________________________________________________________________________________________________________________
 
 
 database("app").collection("Users").findDocument([
-  { user -> user.nome == "Ronaldo" && user.idade > 8, { _id, nome } },
-  { user -> user.idade > 8, { nome, idade } }
+  [ user -> user.nome == "Ronaldo" && user.idade > 8, [_id, nome] ],
+  [ user -> user.idade > 8, [nome, idade] ]
 ]);
 
 database("app").collection("Users").findDocument([
-  { ({ nome, idade }) -> nome == "Ronaldo" && idade > 8, { _id, nome } },
-  { ({ idade }) -> idade > 8, { nome, idade } }
+  [ ({ nome, idade }) -> nome == "Ronaldo" && idade > 8, [_id, nome] ],
+  [ ({ idade }) -> idade > 8, [nome, idade] ]
 ]);
