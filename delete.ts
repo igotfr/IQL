8290f7d5b5c8dca6347355490891fcf9f6f21aad
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2020-11-18 14:26:54 -0300

    Update delete.ts

--- delete.ts
+++ delete.ts
@@ -1,3 +1,17 @@
 database("app").collection("Users").deleteDocument(user -> user.nome == "Ronaldo" && user.idade > 8);
 
 database("app").collection("Users").deleteDocument(({ nome, idade }) -> nome == "Ronaldo" && idade > 8);
+
+___________________________________________________________________________________________________________________________________________________
+
+database("app").collection("Users").deleteDocument([
+  [ user -> user.nome == "Ronaldo" && user.idade > 8, [_id, nome] ],
+  [ user -> user.idade > 8, [nome, idade] ],
+  user -> user.nome == "Meterssaker"
+], [_id, nome]); // for not specified, in this case: "Meterssaker"
+
+database("app").collection("Users").deleteDocument([
+  [ ({ nome, idade }) -> nome == "Ronaldo" && idade > 8, [_id, nome] ],
+  [ ({ idade }) -> idade > 8, [nome, idade] ],
+  ({ nome }) -> nome == "Meterssaker"
+], [_id, nome]); // for not specified, in this case: "Meterssaker"
