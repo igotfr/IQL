60f481eb5e3c12d5e59abde68d047b0759f496a4
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2021-10-03 11:45:39 -0300

    Update and rename ormv.v to orm.v

---
+++ orm.v
@@ -0,0 +1,59 @@
+// connect
+mut db := pg.connect(
+    host: 'localhost'
+    user: 'postgres'
+    password: ''
+    dbname: 'postgres'
+) or { panic(err) }
+
+// create
+db.create('Test', [
+    orm.TableField{
+        name: 'id'
+        typ: serial
+        attrs: [
+            StructAttribute{
+                name: 'primary'
+            },
+            StructAttribute{
+                name: 'sql'
+                has_arg: true
+                kind: .plain
+                arg: 'serial'
+            },
+        ]
+    },
+    orm.TableField{
+        name: 'name'
+        typ: string
+        attrs: []
+    },
+    orm.TableField{
+        name: 'age'
+        typ: int
+    },
+]) or { panic(err) }
+
+// insert in V
+db.insert('Test', orm.QueryData{
+    fields: ['name', 'age']
+    data: ['Louis', 101]
+}) or { panic(err) }
+
+// select
+res := db.@select(orm.SelectConfig{
+    table: 'Test'
+    has_where: true
+    fields: ['id', 'name', 'age']
+    types: [7, 18, 8]
+}, orm.QueryData{}, orm.QueryData{
+    fields: ['name']
+    data: [orm.Primitive('Louis'), i64(101)]
+    types: [18]
+    is_and: [true]
+    kinds: [.eq]
+}) or { panic(err) }
+
+id := res[0][0]
+name := res[0][1]
+age := res[0][2]
