828e45404fbb67ef4a251d02b2f906df784b8885
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2021-10-03 11:41:41 -0300

    Update insert.ts

--- insert.ts
+++ insert.ts
@@ -1,25 +1,23 @@
-database("app").collection("Users").insertDocument(
-{
-  _id: ID(5ds564654fd),
-  nome: "Adalberto",
-  idade: 48
+//db.insert('Users')
+database("app").collection("Users").insertDocument({
+    _id: ID(5ds564654fd),
+    nome: "Adalberto",
+    idade: 48
 }, [_id, nome]); // the second parameter is optional and works like in GraphQL)
 
-database("app").collection("Users").insertDocument([
-{
-  _id: ID(df4547454h),
-  nome: "Ronaldo",
-  idade: 17
+// db.insert('Users')
+database("app").collection("Users").insertDocument([{
+    _id: ID(df4547454h),
+    nome: "Ronaldo",
+    idade: 17
 },
 {
-  _id: ID(gf78hy498g),
-  nome: "Vicente",
-  idade: 32
-}
-], [_id, nome]); // the second parameter is optional and works like in GraphQL)
+    _id: ID(gf78hy498g),
+    nome: "Vicente",
+    idade: 32
+}], [_id, nome]); // the second parameter is optional and works like in GraphQL)
 
-________________________________________________________________________________
-
+// db.insert('Users')
 database("app").collection("Users").insertDocument([
 [
   {
